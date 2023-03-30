03d2e74205df153b1f928709526f3332c7497195
Neel Banga <neelbanga@Neels-MacBook-Air.local>
2023-03-29 22:07:45 -0700

    made piece movement validation methods

--- board_moves.py
+++ board_moves.py
@@ -74,21 +74,130 @@ def create_board():
 
     return board
 
+def check_user_move(board, piece, old_x, old_y, x, y):
 
-# Create all combos with old board and then check it in terms of new board
+    # Create all combos with old board and then check it in terms of new board
 
-def check_pawn():
+    def check_pawn():
+        # Let's make sure there isn't a piece of the same color where said piece wants to move
+        if piece > 0:
+            if board[y][x] > 0:
+                return False
+            else:
+                if board[y][x] < 0:
+                    return False
 
-def check_knight():
+        #  First let's check if it's moving left or right
+        if (old_x + 1 == x and old_y == y) or (old_x - 1 == x and old_y == y):
+            return True
+        
+        # Now let's check if it's moving up or down
+        if (old_y + 1 == y and old_x == x) or (old_y - 1 == y and old_x == x):
+            return True
+        
+        # Now let's check for en passant
+        if (old_x + 1 == x and old_y + 1 == y) or (old_x - 1 == x and old_y + 1 == y):
+            # let's make sure a peice of the opposite color is on that position
+            if new_board[y][x]:
+                if piece > 0:
+                    if board[y][x] < 0:
+                        return True
+                else:
+                    if board[y][x] > 0:
+                        return True
+        
+        return False
 
-def check_bishop():
+    def check_knight():
 
-def check_rook():
+        # Let's make sure there isn't a piece of the same color where said piece wants to move
+        if piece > 0:
+            if board[y][x] > 0:
+                return False
+            else:
+                if board[y][x] < 0:
+                    return False
+                
+        # Check if the knight is moving 1 right/left and 2 up/down
+        if (old_x - 1 == x or old_x + 1 == x) and (old_y + 2 == y or old_y - 2 == y):
+            return True
+        
+        # Check if the knight is moving 2 right/left and 1 up/down
+        if (old_x - 2 == x or old_x + 2 == x) and (old_y + 1 == y or old_y - 1 == y):
+            return True
+        
+        return False
 
-def check_queen():
+    def check_bishop():
 
+        for i in range(1, 8):
+            if old_x + i == x and old_y + i == y:
 
-def check_user_move(board, piece, x, y):
+                if piece > 0:
+                    if board[y][x] > 0:
+                        return False
+                else:
+                    if board[y][x] < 0:
+                        return False
+    
+                return True
+            
+        return False
+
+    def check_rook():
+
+        # Let's make sure there isn't a piece of the same color where said piece wants to move
+        if piece > 0:
+            if board[y][x] > 0:
+                return False
+            else:
+                if board[y][x] < 0:
+                    return False
+
+        for i in range(1, 8):
+            if old_x + i == x and old_y == y:
+
+                if piece > 0:
+                    if board[y][x] > 0:
+                        return False
+                else:
+                    if board[y][x] < 0:
+                        return False
+    
+                return True
+            
+        return False
+
+    def check_queen():
+
+        if check_bishop == True:
+            return True
+        
+        if check_rook == True:
+            return True
+
+    def check_king():
+
+        # Let's make sure there isn't a piece of the same color where said piece wants to move
+        if piece > 0:
+            if board[y][x] > 0:
+                return False
+            else:
+                if board[y][x] < 0:
+                    return False
+
+        # We can simply copy some of the conditionals of our pawn method
+        #  First let's check if it's moving left or right
+        if (old_x + 1 == x and old_y == y) or (old_x - 1 == x and old_y == y):
+            return True
+        
+        # Now let's check if it's moving up or down
+        if (old_y + 1 == y and old_x == x) or (old_y - 1 == y and old_x == x):
+            return True
+        
+        # Now let's check for diagonal movement
+        if (old_x + 1 == x and old_y + 1 == y) or (old_x - 1 == x and old_y + 1 == y):
+            return True
 
     if not x <= 7 and not x >= 0:
         return False
@@ -112,17 +221,24 @@ def check_user_move(board, piece, x, y):
     new_board[y][x] = piece
 
     if piece == 1: # pawn
+        check_pawn(board, new_board)
 
     elif piece == 3: # knight
+        check_knight(board, new_board)
 
     elif piece == 3.5: # bishop
+        check_bishop(board, new_board)
     
     elif piece == 5: # rook
+        check_rook(board, new_board)
 
     elif piece == 9: # queen
+        check_queen(board, new_board)
 
-    else: # king
-
+    else:
+        check_king(board, new_board)
+    
+    return True
 
     # Here let's check if that value of y,x has a piece (meaning it has to be of the other side)
     # We have to do something with that value
